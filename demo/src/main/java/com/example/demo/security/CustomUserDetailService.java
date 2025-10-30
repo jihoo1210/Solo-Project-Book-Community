20c86466baf1724b30bab562a3e77fa30e281d75
jihoo1210 <jihoostudy1@gmail.com>
2025-10-30 15:26:29 +0900

    resolves #3 보안 로직 구현 완성
    * Close #1 백엔드 초기 설정 완료
    
    * #3 Json Web Token 추가
    
    * #3 JWT 생성, 검사, 유효성 검증 기능 추가
    
    * #3 UserDetail 구현체 추가
    
    * #3 UserDetailService 구현체 추가
    
    * #3 JWT 필터 생성
    
    * #3 스프링 시큐리티 설정
    
    ---------
    
    Co-authored-by: ParkJihoo <jihoosyudy1@gmail.com>

---
+++ demo/src/main/java/com/example/demo/security/CustomUserDetailService.java
@@ -0,0 +1,21 @@
+package com.example.demo.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByUsername(username)
+                .map(CustomUserDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username))
+    }
+}
---
+++ demo/src/main/java/com/example/demo/security/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.example.demo.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.CorsFilter;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    public final JwtAuthorizationFilter jwtAuthorizationFilter;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(c -> c.disable())
+                .cors(c -> c.disable())
+                .httpBasic(h -> h.disable())
+                .sessionManagement(sessoin -> sessoin
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .headers(h -> h.frameOptions(f -> f.sameOrigin()))
+                .authorizeHttpRequests(a -> a
+                        .requestMatchers("/auth/**", "/h2-console/**").permitAll()
+                        .requestMatchers("/email/**").hasRole("TEMP")
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .anyRequest().hasRole("USER"))
+                .addFilterAfter(
+                        jwtAuthorizationFilter, CorsFilter.class
+                );
+        return http.build();
+    }
+}
