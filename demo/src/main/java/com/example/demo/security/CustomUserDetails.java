4c850e9c0203a251922f53d8064c4686fe5040ae
ParkJihoo <jihoosyudy1@gmail.com>
2025-10-30 14:58:12 +0900

    #3 UserDetail 구현체 추가

---
+++ demo/src/main/java/com/example/demo/security/CustomUserDetails.java
@@ -0,0 +1,60 @@
+package com.example.demo.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        if(user.getRole().equals(ROLE.ROLE_ADMIN)) {
+            authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+            authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+        } else if(user.getRole().equals(ROLE.ROLE_USER)) {
+            authorities.add(new SimpleGrantedAuthority("ROLE_USER"))
+        } else {
+            authorities.add(new SimpleGrantedAuthority("ROLE_TEMP"))
+        }
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
