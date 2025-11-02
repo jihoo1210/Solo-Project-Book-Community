50af227d81a4a5b8fa09e1fb2d9e038995d82810
jihoo1210 <jihoostudy1@gmail.com>
2025-11-02 22:44:58 +0900

    #1 User Entity, Dto, Service, Repository

---
+++ demo/src/main/java/com/example/demo/dto/auth/SignupRequest.java
@@ -0,0 +1,24 @@
+package com.example.demo.dto.auth;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class SignupRequest {
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String password;
+}
---
+++ demo/src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,14 @@
+package com.example.demo.service;
+
+import com.example.demo.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository repository;
+}
